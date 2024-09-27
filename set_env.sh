b122de71cd1339cd52f8f3f36ca29c83f5e02856
Huynh Thanh Long <huynhthanhlong.dev.it@gmail.com>
2024-09-27 22:57:46 +0700

    Inital project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+export POSTGRES_USERNAME=thanhLong
+export POSTGRES_PASSWORD=thanhlong6720
+export POSTGRES_HOST=cdr.cpfarcgdj2ls.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=cdr
+export AWS_BUCKET=arn:aws:s3:::monolithudacityprokect3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export AWS_ACCESS_KEY_ID=AKIA2544BH7VDIATAJ75
+export AWS_SECRET_ACCESS_KEY=O6kpFquYMzrMXNMgXW7llAW7T8XNWi1fvAkK
+export JWT_SECRET=testing
+export URL=http://localhost:8100
