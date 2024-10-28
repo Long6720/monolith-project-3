930e1039bf7cb7844d2a2e37f2a239c4a0febc58
Huynh Thanh Long <huynhthanhlong.dev.it@gmail.com>
2024-10-28 10:24:38 +0700

    add some module

--- udagram-frontend/src/app/app.module.ts
+++ udagram-frontend/src/app/app.module.ts
@@ -1,36 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { MenubarComponent } from './menubar/menubar.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { MenubarComponent } from "./menubar/menubar.component";
 
-import { AuthModule } from './auth/auth.module';
-import { ApiService } from './api/api.service';
+import { AuthModule } from "./auth/auth.module";
+import { ApiService } from "./api/api.service";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenubarComponent
-  ],
+  declarations: [AppComponent, MenubarComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [
     ApiService,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
