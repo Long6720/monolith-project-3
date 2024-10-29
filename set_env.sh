a4c1929766d21a3b23c79ead31dc9529cd483c7f
Huỳnh Thanh Long <81316439+Long6720@users.noreply.github.com>
2024-10-29 10:10:11 +0700

    update eslint (#4)

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 export POSTGRES_USERNAME=thanhLong
 export POSTGRES_PASSWORD=thanhlong6720
-export POSTGRES_HOST=cdr.cpfarcgdj2ls.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=cdr.cger0lfcbst5.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=cdr
-export AWS_BUCKET=arn:aws:s3:::monolithudacityprokect3
+export AWS_BUCKET=arn:aws:s3:::monolith-project-3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export AWS_ACCESS_KEY_ID=AKIA2544BH7VDIATAJ75
-export AWS_SECRET_ACCESS_KEY=O6kpFquYMzrMXNMgXW7llAW7T8XNWi1fvAkK
+export AWS_ACCESS_KEY_ID=AKIAX2V7CRVSMKQNPZJR
+export AWS_SECRET_ACCESS_KEY=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
 export JWT_SECRET=testing
 export URL=http://localhost:8100
